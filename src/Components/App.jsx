d85473223e8f2f0e71aba644136a52e7c4e9817f
Nishanth <nishanth1596@gmail.com>
2025-01-23 16:30:51 +0530

    fixed accessibility issue

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -11,18 +11,23 @@ const App = () => {
         <Header />
         <Main />
         <Footer />
+        <figure>
+          <img
+            className="absolute -top-[6rem] left-[25rem] -z-50 hidden md:block lg:hidden"
+            src={heroImgTablet}
+            alt="A woman learning from her laptop while having a hot coffee"
+          />
 
-        <img
-          className="absolute -top-[6rem] left-[25rem] -z-50 hidden md:block lg:hidden"
-          src={heroImgTablet}
-          alt="A woman learning from her laptop while having a hot coffee"
-        />
+          <img
+            className="absolute -z-50 hidden md:hidden lg:hidden xl:-top-[15rem] xl:left-[46rem] xl:block"
+            src={heroImgDesktop}
+            alt="A woman learning from her laptop while having a hot coffee"
+          />
 
-        <img
-          className="absolute -z-50 hidden md:hidden lg:hidden xl:-top-[15rem] xl:left-[46rem] xl:block"
-          src={heroImgDesktop}
-          alt="A woman learning from her laptop while having a hot coffee"
-        />
+          <figcaption>
+            A woman learning from her laptop while having a hot coffee
+          </figcaption>
+        </figure>
       </div>
     </div>
   );
